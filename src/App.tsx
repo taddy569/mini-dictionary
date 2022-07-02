62352dd25f8e3e6195a6b05c021d0be58e9266bd
bendev <bendev5369@gmail.com>
2022-07-03 00:16:33 +0700

    add redux-persist

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,22 @@
 import React from 'react'
 
 import { Provider } from 'react-redux'
+import { PersistGate } from 'redux-persist/integration/react'
 
 import AppRouter from 'routers'
-import { store } from 'redux/store'
+import { store, persistedStore } from 'redux/store'
 import { WrapperApp } from './App.style'
 
 function App() {
   return (
     <Provider store={store}>
-      <WrapperApp>
-        <AppRouter />
-      </WrapperApp>
+      <PersistGate loading={null} persistor={persistedStore}>
+        <WrapperApp>
+          <AppRouter />
+        </WrapperApp>
+      </PersistGate>
     </Provider>
   )
 }
 
 export default App
-
-// https://dummyjson.com/docs/products
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,16 +1,31 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { configureStore, combineReducers } from '@reduxjs/toolkit'
 import logger from 'redux-logger'
+
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
 
 import { productsReducer, cartsReducer, authReducer } from './slices'
 
+const persistConfig = {
+  key: 'root',
+  storage: storage,
+  whitelist: ['auth'],
+}
+
+const rootReducer = combineReducers({
+  products: productsReducer,
+  carts: cartsReducer,
+  auth: authReducer,
+})
+
+const persistedRootReducer = persistReducer(persistConfig, rootReducer)
+
 export const store = configureStore({
-  reducer: {
-    products: productsReducer,
-    carts: cartsReducer,
-    auth: authReducer,
-  },
+  reducer: persistedRootReducer,
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
 })
+
+export const persistedStore = persistStore(store)
 
 export type RootState = ReturnType<typeof store.getState>
 export type AppDispatch = typeof store.dispatch
